1efcfbf03e01db21b56d0b4a998a72110a98691f
julie <7jujubuilds@gmail.com>
2025-11-07 20:40:38 +0900

    chore: Delete database files and install MySQL connector

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO "USERS" (email, password) VALUES ('a@gmail.com', 'a');
-INSERT INTO "DRAGONS" (name, breed, gender, primary_gene, secondary_gene, tertiary_gene, primary_color, secondary_color, tertiary_color, eye_type, flight) VALUES ('Kesler', 'Spiral', 'M', 'Petals', 'Bee', 'Filigree', 'Carmine', 'Maize', 'Emerald', 'Common', 'Wind');
-INSERT INTO "GOALS" (breed, gender, primary_gene, secondary_gene, tertiary_gene, primary_color, secondary_color, tertiary_color, eye_type, flight) VALUES ('Skydancer', 'M', 'Iridescent', 'Flair', 'Underbelly', 'Violet', 'Blush', 'Moon', 'Common', 'Shadow');

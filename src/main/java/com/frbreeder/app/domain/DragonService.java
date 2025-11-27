137a839be924711cdc713c065476210247921598
julie <7jujubuilds@gmail.com>
2025-11-27 11:25:09 +0900

    refactor: Apply logging and proper exception handling to DragonService

--- src/main/java/com/frbreeder/app/domain/DragonService.java
+++ src/main/java/com/frbreeder/app/domain/DragonService.java
@@ -1,6 +1,6 @@
 package com.frbreeder.app.domain;
 
-import com.frbreeder.app.common.error.InvalidRequestException;
+import com.frbreeder.app.common.error.NotFoundException;
 import com.frbreeder.app.domain.common.FrColor;
 import com.frbreeder.app.domain.entity.Breed;
 import com.frbreeder.app.domain.entity.Dragon;
@@ -22,11 +22,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class DragonService {
+
+    private static final Logger log = LoggerFactory.getLogger(DragonService.class);
 
     private final DragonRepository dragonRepository;
     private final BreedRepository breedRepository;
@@ -34,7 +38,9 @@ public class DragonService {
     private final SecondaryGeneRepository secondaryGeneRepository;
     private final TertiaryGeneRepository tertiaryGeneRepository;
 
-    public DragonService(final DragonRepository dragonRepository, final BreedRepository breedRepository, final PrimaryGeneRepository primaryGeneRepository, final SecondaryGeneRepository secondaryGeneRepository, final TertiaryGeneRepository tertiaryGeneRepository) {
+    public DragonService(final DragonRepository dragonRepository, final BreedRepository breedRepository,
+                         final PrimaryGeneRepository primaryGeneRepository, final SecondaryGeneRepository secondaryGeneRepository,
+                         final TertiaryGeneRepository tertiaryGeneRepository) {
         this.dragonRepository = dragonRepository;
         this.breedRepository = breedRepository;
         this.primaryGeneRepository = primaryGeneRepository;
@@ -50,17 +56,6 @@ public class DragonService {
                         .map(this::getRosterDragon)
                         .toList()
         );
-    }
-
-    @Transactional
-    public RosterDragons addDragons(final Workspace workspace, final NewDragonRequest request) {
-        List<RosterDragon> rosterDragons = new ArrayList<>();
-        for (NewDragon requestDragon : request.dragons()) {
-            Dragon dragon = parseScryUrl(requestDragon.frId(), requestDragon.name(), requestDragon.scryUrl(), workspace);
-            Dragon newDragon = dragonRepository.save(dragon);
-            rosterDragons.add(getRosterDragon(newDragon));
-        }
-        return new RosterDragons(rosterDragons);
     }
 
     private RosterDragon getRosterDragon(final Dragon dragon) {
@@ -79,7 +74,20 @@ public class DragonService {
         );
     }
 
+    @Transactional
+    public RosterDragons addDragons(final Workspace workspace, final NewDragonRequest request) {
+        List<RosterDragon> rosterDragons = new ArrayList<>();
+        for (NewDragon requestDragon : request.dragons()) {
+            Dragon dragon = parseScryUrl(requestDragon.frId(), requestDragon.name(), requestDragon.scryUrl(), workspace);
+            Dragon newDragon = dragonRepository.save(dragon);
+            rosterDragons.add(getRosterDragon(newDragon));
+        }
+        return new RosterDragons(rosterDragons);
+    }
+
     private Dragon parseScryUrl(final Long frId, final String name, final String url, final Workspace workspace) {
+        log.info("[SERVICE] workspaceId: {}, frId: {}, scryUrl: {}", workspace.getId(), frId, url);
+
         String queryParams = url.substring(url.indexOf("?") + 1);
         String[] queryPairs = queryParams.split("&");
         Map<String, Integer> queries = new HashMap<>();
@@ -114,31 +122,30 @@ public class DragonService {
 
     private PrimaryGene getPrimaryGeneFromScry(final int geneId) {
         return primaryGeneRepository.findById(geneId)
-                .orElseThrow();
+                .orElseThrow(() -> new NotFoundException("This primary gene is not in the database."));
     }
 
     private SecondaryGene getSecondaryGeneFromScry(final int geneId) {
         return secondaryGeneRepository.findById(geneId)
-                .orElseThrow();
+                .orElseThrow(() -> new NotFoundException("This secondary gene is not in the database."));
     }
 
     private TertiaryGene getTertiaryGeneFromScry(final int geneId) {
         return tertiaryGeneRepository.findById(geneId)
-                .orElseThrow();
+                .orElseThrow(() -> new NotFoundException("This tertiary gene is not in the database."));
     }
 
     @Transactional(readOnly = true)
     public RegisteredDragon getDragon(final Long id, final Long workspaceId) {
         Dragon dragon = dragonRepository.findByIdAndWorkspaceId(id, workspaceId)
-                .orElseThrow(() -> new InvalidRequestException("The dragon by that id does not exist."));
+                .orElseThrow(() -> new NotFoundException("The dragon by that id does not exist."));
         return new RegisteredDragon(dragon.getScryUrl(), dragon.getName(), id);
     }
 
     @Transactional
     public void deleteDragon(final Long id, final Long workspaceId) {
         Dragon dragon = dragonRepository.findByIdAndWorkspaceId(id, workspaceId)
-                .orElseThrow(() -> new InvalidRequestException("The dragon by that id does not exist."));
-
+                .orElseThrow(() -> new NotFoundException("The dragon by that id does not exist."));
         dragonRepository.deleteById(dragon.getId());
     }
 
